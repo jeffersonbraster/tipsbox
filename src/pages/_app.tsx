7bdb9296271d4193f0757bb56151d3e2dedd7216
Jefferson <jeffersonbraster@gmail.com>
2021-12-27 22:07:19 -0300

    correção do title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </Layout>
       <Head>
-        <title>Fanime</title>
+        <title>Tips-Box</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
